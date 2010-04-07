86ca8f0dc1195c2668ae1e041d32f877ce6478e8
Emil Palm <emil@emilpalm.(none)>
2010-04-07 11:46:42 +0100

    first commit

---
+++ BaseTable.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 7, 2010
+
+@author: emil
+'''
+
+class BaseTable(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
